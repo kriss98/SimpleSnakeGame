0b2081bb8bc698565abcafcb0f85705b5c59ea6a
Kristofar Hristov <hristov.kristofar@gmail.com>
2020-10-30 17:17:26 +0200

    Add updateScreen method

--- SimpleSnakeGame/Form1.cs
+++ SimpleSnakeGame/Form1.cs
@@ -16,9 +16,51 @@ namespace SimpleSnakeGame
 
         private Circle food = new Circle();
 
+        private Settings settings = new Settings();
+
         public Form1()
         {
             InitializeComponent();
+
+            this.gameTimer.Interval = 1000 / this.settings.Speed;
+            this.gameTimer.Tick += updateScreen;
+            this.gameTimer.Start();
+
+            this.startGame();
+        }
+
+        private void updateScreen(Object sender, EventArgs args)
+        {
+            if (this.settings.GameOver)
+            {
+                if (Input.KeyPress(Keys.Enter))
+                {
+                    this.startGame();
+                }
+            }
+            else
+            {
+                if (Input.KeyPress(Keys.Right) && this.settings.Direction != Direction.Left)
+                {
+                    this.settings.Direction = Direction.Right;
+                }
+                else if (Input.KeyPress(Keys.Left) && this.settings.Direction != Direction.Right)
+                {
+                    this.settings.Direction = Direction.Left;
+                }
+                else if (Input.KeyPress(Keys.Up) && this.settings.Direction != Direction.Down)
+                {
+                    this.settings.Direction = Direction.Up;
+                }
+                else if (Input.KeyPress(Keys.Down) && this.settings.Direction != Direction.Up)
+                {
+                    this.settings.Direction = Direction.Down;
+                }
+
+                this.movePlayer();
+            }
+
+            this.pbCanvas.Invalidate();
         }
 
         private void Form1_Load(object sender, EventArgs e)
--- SimpleSnakeGame/Settings.cs
+++ SimpleSnakeGame/Settings.cs
@@ -1,6 +1,37 @@
 ﻿namespace SimpleSnakeGame
 {
+    public enum Direction
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+    }
     public class Settings
     {
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public int Speed { get; set; }
+
+        public int Score { get; set; }
+
+        public int Points { get; set; }
+
+        public bool GameOver { get; set; }
+
+        public Direction Direction { get; set; }
+
+        public Settings()
+        {
+            this.Width = 16;
+            this.Height = 16;
+            this.Speed = 20;
+            this.Score = 0;
+            this.Points = 100;
+            this.GameOver = false;
+            this.Direction = Direction.Down;
+        }
     }
 }
